e9e9f0efb4bfdbc6b65a3b4314ddcd4f8d59bb59
Doggenn <19arg09@gmail.com>
2024-04-17 13:12:40 +0200

    Añadidos botones del nav e icono para la pestaña

--- src/assets/Pages/Menu/Menu.jsx
+++ src/assets/Pages/Menu/Menu.jsx
@@ -9,7 +9,12 @@ export default function Menu() {
             <div className='MenuStilo'>
                 <header>
                     <nav>
-                        <Link className='estiloNav' to="/Home">Home</Link>
+                        <Link className='estiloNav' to="/Home"><img src="./images/Icons/icons-blue/home@3x.png" alt="" /></Link>
+                        <Link className='estiloNav' to="/Home"><img src="./images/Icons/icons-blue/mapa@3x.png" alt="" /></Link>
+                        <Link className='estiloNav' to="/Home"><img src="./images/Icons/icons-blue/mascota@3x.png" alt="" /></Link>
+                        <Link className='estiloNav' to="/Home"><div><img src="./images/foto-perfil/fotodeperfil@3x.png" alt="" /></div></Link>
+                        
+                        <Link className='estiloNav' to="/Home"><img src="./images/Icons/icons-blue/mS@3x.png" alt="" /></Link>
                     </nav>
                 </header>
             </div>
