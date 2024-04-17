286f644cbe8dafb2d8d5e1ba3683d6e6111b963b
natbrow <145482189+natbrow@users.noreply.github.com>
2024-04-17 15:30:29 +0200

    imagenes restantes

--- src/assets/Components/Perfil/Perfil.jsx
+++ src/assets/Components/Perfil/Perfil.jsx
@@ -2,6 +2,44 @@ import React from 'react'
 
 export default function Perfil() {
     return (
-        <div>Perfil</div>
+        <div>
+            <div>
+                <img src="public/images/foto-perfil/fotodeperfil@3x.png" alt="" />
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <img src="public/images/foto-perfil/chica@3x.png" alt="" />
+                        <h4>Mi Perfil</h4>
+                    </div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
     )
 }
