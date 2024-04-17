103679f9ee4977dd2a8e241ee57f3eba859e8c7c
natbrow <145482189+natbrow@users.noreply.github.com>
2024-04-17 18:21:00 +0200

    imagenes nuevas

--- src/assets/Components/Perfil/Perfil.jsx
+++ src/assets/Components/Perfil/Perfil.jsx
@@ -13,31 +13,63 @@ export default function Perfil() {
                         <img src="public/images/foto-perfil/chica@3x.png" alt="" />
                         <h4>Mi Perfil</h4>
                     </div>
-                    <div></div>
+                    <div>
+                        <img src="public/images/Icons/icons-pink/arrow@3x.png" alt="" />
+                    </div>
                 </div>
                 <div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <img src="public/images/Icons/icons-blue/localization@3x.png" alt="" />
+                        <h4>Direcciones</h4>
+                    </div>
+                    <div>
+                        <img src="public/images/Icons/icons-pink/arrow@3x.png" alt="" />
+                    </div>
                 </div>
                 <div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <img src="public/images/Icons/icons-blue/favoritosCopyblue@3x.png" alt="" />
+                        <h4>Favoritos</h4>
+                    </div>
+                    <div>
+                        <img src="public/images/Icons/icons-pink/arrow@3x.png" alt="" />
+                    </div>
                 </div>
                 <div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <img src="public/images/Icons/icons-blue/notificacionesblue@3x.png" alt="" />
+                        <h4>Notificaciones</h4>
+                    </div>
+                    <div>
+                        <img src="public/images/Icons/icons-pink/arrow@3x.png" alt="" />
+                    </div>
                 </div>
                 <div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <img src="public/images/Icons/icons-blue/apadrina@3x.png" alt="" />
+                        <h4>Estado de la adopción</h4>
+                    </div>
+                    <div>
+                        <img src="public/images/Icons/icons-pink/arrow@3x.png" alt="" />
+                    </div>
                 </div>
                 <div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <img src="public/images/Icons/icons-blue/apadrina@3x.png" alt="" />
+                        <h4>Estado de la adopción</h4>
+                    </div>
+                    <div>
+                        <img src="public/images/Icons/icons-pink/arrow@3x.png" alt="" />
+                    </div>
                 </div>
                 <div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <img src="public/images/Icons/icons-blue/apadrina@3x.png" alt="" />
+                        <h4>Estado de la adopción</h4>
+                    </div>
+                    <div>
+                        <img src="public/images/Icons/icons-pink/arrow@3x.png" alt="" />
+                    </div>
                 </div>
             </div>
         </div>
