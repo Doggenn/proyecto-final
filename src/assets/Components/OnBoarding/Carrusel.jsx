89001751ba31817f2953a8dec5938a46c9122cb2
natbrow <145482189+natbrow@users.noreply.github.com>
2024-04-21 13:18:50 +0200

    css formulario

--- src/assets/Components/OnBoarding/Carrusel.jsx
+++ src/assets/Components/OnBoarding/Carrusel.jsx
@@ -1,13 +1,20 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useEffect } from 'react'
 import OnBoardingOne from './OnBoardingOne'
 import OnBoardingTwo from './OnBoardingTwo'
 import OnBoardingThree from './OnBoardingThree'
 import Carousel from 'react-bootstrap/Carousel';
+import { Link } from 'react-router-dom';
 
 export default function Carrusel() {
+
     return (
         <div>
+            <div className='b-uton'>
+                <Link to={'/Home'} className="btn-close " />
+
+
+            </div>
             <Carousel data-bs-theme="dark">
                 <Carousel.Item>
                     <OnBoardingOne />
